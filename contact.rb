70bf75d2120b92aceca7013b681d06734eb5e10f
Jessica <jhai_alex@hotmail.com>
2016-10-31 15:07:20 -0400

    added activerecord and minirecord

--- contact.rb
+++ contact.rb
@@ -4,7 +4,11 @@
 #   ruby test/contact_test.rb
 #
 # If your implementation of the Contact class is 'right', then you should see that all tests pass!
-require 'pry'
+gem 'activerecord', '=4.2.7'
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm-web.sqlite3')
 
 class Contact
 
